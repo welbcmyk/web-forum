8139143f34c7d7debde6c5dce21db032377eb05d
welbcmyk <r.weineck@hotmail.de>
2020-06-07 20:46:42 +0200

    included bootstrap

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
