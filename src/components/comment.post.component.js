01c72b3b1a09ff3d449f40981525759a849748f3
welbcmyk <r.weineck@hotmail.de>
2020-06-12 09:51:22 +0200

    init comment component

---
+++ src/components/comment.post.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class HomeFeed extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to a Comment</p>
+            </div>
+        )
+    }
+}
