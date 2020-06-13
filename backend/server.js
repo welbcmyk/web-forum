3877e8f3ded920e7de0a0f2bd9a348e16c80bb81
welbcmyk <r.weineck@hotmail.de>
2020-06-13 18:51:01 +0200

    backend init

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+/*mongoose
+  .connect("mongodb://mongo:27017/forum-web-app", { useNewUrlParser: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));*/
+
+// TODO Routes
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
