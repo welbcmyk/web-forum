d70c571cf0455f10a67dd7b766f9ac7e2f9380b7
welbcmyk <r.weineck@hotmail.de>
2020-06-25 13:35:00 +0200

    changed database

---
+++ backend/routes/login.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const User = require('../models/user.model');
+
+
+router.route('/').get((req, res) => {
+    User.findOne({username: req.body.username})
+      .then(user => res.json(users))
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+  
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -10,15 +10,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = process.env.ATLAS_URI;
 mongoose
-  .connect("mongodb://mongo:27017/forum-web-app", { useNewUrlParser: true })
+  .connect(uri, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
   .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log("MongoDb connection error: " + err));
 
 const usersRoute = require('./routes/users');
 const postsRoute = require('./routes/posts');
 const commentsRoute = require('./routes/comments');
 const forumsRoute = require('./routes/forums');
+const loginRoute = require('./routes/login');
 
 app.use('/users', usersRoute);
 app.use('/posts', postsRoute);
@@ -28,6 +30,7 @@ app.use('/user', usersRoute);
 app.use('/post', postsRoute);
 app.use('/comment', commentsRoute);
 app.use('/forum', forumsRoute);
+app.use('/login', loginRoute);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
