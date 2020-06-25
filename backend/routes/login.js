8bed0a33609bb8dbef58aa1df7791b5b0dc8bdb3
welbcmyk <r.weineck@hotmail.de>
2020-06-25 16:33:59 +0200

    login finds combination of username and password

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const User = require("../models/user.model");
 
 router.route("/").post((req, res) => {
-  User.findOne({ username: req.body.username })
+  User.findOne({ username: req.body.username, password: req.body.password })
     .then((user) => res.json(user))
     .catch((err) => res.status(400).json("Error: " + err));
 });
