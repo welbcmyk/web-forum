0bf46dd3b6e3fbc8f13dafea41c0bdfeb310b195
welbcmyk <r.weineck@hotmail.de>
2020-06-26 18:03:22 +0200

    bugfix

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const User = require("../models/user.model");
 
 router.route("/").post((req, res) => {
-  User.findOne({ username: req.body.username, password: req.body.password })
+  User.findOne({ $and: [{username: req.body.username}, { password: req.body.password }]})
     .then((user) => {
       if(user == null){
         return res.status(400).json("Illegal Combination");
