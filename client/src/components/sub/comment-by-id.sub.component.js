b3812eb44bdba0cfee332aaffd9e6b2fece3056a
welbcmyk <r.weineck@hotmail.de>
2020-06-27 17:37:40 +0200

    added handlebar for comments

--- client/src/components/sub/comment-by-id.sub.component.js
+++ client/src/components/sub/comment-by-id.sub.component.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import axios from "axios";
 import backendAddress from "../../helpers/backend-address";
 import { withRouter } from "react-router-dom";
+import { Modal, Button } from "react-bootstrap";
+import { authenticationService } from "../../services/authentication.service";
 
 import CommentComp from "./comment.sub.component";
 
@@ -10,13 +12,18 @@ class Comment extends Component {
         super(props);
 
         this.showUser = this.showUser.bind(this);
+        this.handleCloseDeletePopUp = this.handleCloseDeletePopUp.bind(this);
+        this.handleShowDeletePopUp = this.handleShowDeletePopUp.bind(this);
+        this.deleteComment = this.deleteComment.bind(this);
+
         this.state = {
             id: "",
             userid: "",
             postid: "",
             username: "",
             date: "",
-            body: ""
+            body: "",
+            showDeletePopUp: false,
         }
     }
 
@@ -54,16 +61,65 @@ class Comment extends Component {
       this.props.history.push("/user/" + this.state.username);
     }
 
+    editComment(e){
+        e.stopPropagation();
+        this.props.history.push("/comment/edit/" + this.state._id);
+    }
+
+    editComment(e){
+        e.stopPropagation();
+    }
+
+    handleCloseDeletePopUp() {
+        this.setState({
+            showDeletePopUp: false,
+        })
+    }
+
+    handleShowDeletePopUp() {
+        this.setState({
+            showDeletePopUp: true,
+        })
+    }
+
+    deleteComment() {
+        axios.delete(backendAddress() + "/comment/" + this.state.id)
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+        .finally(() => this.props.history.go());
+    }
+
     render(){
         return (
-          <CommentComp
-            id={this.state._id}
-            userName={this.state.username}
-            date={this.state.date}
-            body={this.state.body}
-            editComment={() => this.props.history.push("/comment/edit/" + this.state._id)}
-            showUser={this.showUser}
-          />
+            <>
+                <CommentComp
+                    id={this.state._id}
+                    userName={this.state.username}
+                    date={this.state.date}
+                    body={this.state.body}
+                    editComment={this.editComment}
+                    showUser={this.showUser}
+                    ownComment={this.state.userid == authenticationService.currentUserValue._id}
+                    deleteComment={this.handleShowDeletePopUp}
+                />
+                <Modal
+                  show={this.state.showDeletePopUp}
+                  onHide={this.handleCloseDeletePopUp}
+                >
+                  <Modal.Header closeButton>
+                    <Modal.Title>Deleting Comment</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>Are you sure you want to delete this Comment?</Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="secondary" onClick={this.handleCloseDeletePopUp}>
+                      No
+                    </Button>
+                    <Button variant="danger" onClick={this.deleteComment}>
+                      Yes
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
+            </>
         );
     }
 }
--- client/src/components/sub/comment.sub.component.js
+++ client/src/components/sub/comment.sub.component.js
@@ -14,15 +14,24 @@ function Comment(props) {
             {props.date}
           </div>
           <div style={{height:"auto", width:"50rem"}}>{props.body}</div>
-          <div>
-            {props.ownComment ? (
-              <i onClick={props.editComment} class="fa fa-pencil"></i>
-            ) : (
-              ""
-            )}
-          </div>
         </div>
       </Card.Body>
+      <Card.Footer>
+        <div>
+          {props.ownComment ? (
+            <i onClick={props.editComment} style={{margin: "0px 20px"}} class="fa fa-pencil  float-right"></i>
+          ) : (
+            null
+          )}
+        </div>
+        <div>
+          {props.ownComment ? (
+            <i onClick={props.deleteComment} style={{margin: "0px 20px"}} class="fa fa-trash  float-right"></i>
+          ) : (
+            null
+          )}
+        </div>
+      </Card.Footer>
     </Card>
   );
 }
